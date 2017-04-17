189d25ba3525498ce66d9eaa1ce66262a6bb8eeb
Toshi7 <ttoshi0719@gmail.com>
2017-04-17 17:48:29 +0900

    add more features

--- PostgressSQLTest/MainWindow.xaml.cs
+++ PostgressSQLTest/MainWindow.xaml.cs
@@ -12,8 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 using Npgsql;
+using System.Data;
 
 namespace PostgressSQLTest
 {
@@ -22,6 +22,8 @@ namespace PostgressSQLTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        NpgsqlConnection conn;
+        NpgsqlCommand cmd;
 
         public MainWindow()
         {
@@ -29,12 +31,12 @@ namespace PostgressSQLTest
 
             InitializeComponent();
 
-            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;Database=test;User Id=postgres;" + "Password=admin;");
+            conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;Database=test;User Id=postgres;" + "Password=admin;");
             conn.Open();
 
 
             // Define a query
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM simple_table", conn);
+            cmd = new NpgsqlCommand("SELECT * FROM simple_table", conn);
 
 
             // Execute a query
@@ -44,7 +46,9 @@ namespace PostgressSQLTest
             while (dr.Read())
                //    items.Add(new User() { id = (int)dr[0], tekst = (String)(dr[1])});
 
-                   items.Add(new User() { id = Convert.ToInt32(dr[0]), tekst = Convert.ToString(dr[1]) });
+                   items.Add(new User() { id = Convert.ToInt32(dr[0]), tekst = Convert.ToString(dr[1]),
+                   gender = Convert.ToString(dr[2])
+                   });
                     // Read all rows and output the first column in each row
             //                                Console.Write("{0}\n", dr[0]);
             //            Console.ReadLine();
@@ -54,10 +58,6 @@ namespace PostgressSQLTest
             // Close connection
             conn.Close();
 
-            
-
-
-
         }
 
         public class User
@@ -66,9 +66,113 @@ namespace PostgressSQLTest
 
             public String tekst { get; set; }
 
-            public string Mail { get; set; }
+            public string gender { get; set; }
 
         }
 
+
+        private void BindGrid()
+        {
+            NpgsqlConnection conn;
+            NpgsqlCommand cmd;
+            
+            //if (con.State != ConnectionState.Open)
+            conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;Database=test;User Id=postgres;" + "Password=admin;");
+            conn.Open();
+
+            cmd = new NpgsqlCommand("SELECT name FROM simple_table", conn);
+            // Execute the query and obtain a result set
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            /*if (dr.HasRows)
+            {
+                lblCount.Visibility = System.Windows.Visibility.Hidden;
+                gvData.Visibility = System.Windows.Visibility.Visible;
+
+            }
+            else
+            {
+                lblCount.Visibility = System.Windows.Visibility.Visible;
+                gvData.Visibility = System.Windows.Visibility.Hidden;
+            }*/
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;Database=test;User Id=postgres;" + "Password=admin;");
+            conn.Open();
+            cmd.Connection = conn;
+
+            if (txtEmpId.Text != "")
+            {
+                if (txtEmpId.IsEnabled == true)
+                {
+                    if (ddlGender.Text != "Select Gender")
+                    {
+                        cmd.CommandText = "insert into simple_table(id,name,gender) Values(" + txtEmpId.Text + ",'" + txtEmpName.Text + "','" + ddlGender.Text + "')";
+                       cmd.ExecuteNonQuery();
+                        BindGrid();
+                        MessageBox.Show("Employee Added Successfully...");
+                        //ClearAll();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please Select Gender Option....");
+                    }
+                }
+                else
+                {
+                    cmd.CommandText = "update tbEmp set EmpName='" + txtEmpName.Text + "',Gender='" + ddlGender.Text + "',Contact=" + txtContact.Text + ",Address='" + txtAddress.Text + "' where EmpId=" + txtEmpId.Text;
+                    cmd.ExecuteNonQuery();
+                    BindGrid();
+                    MessageBox.Show("Employee Details Updated Succesffully...");
+                    //ClearAll();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Add Employee Id.......");
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvUsers.SelectedItems.Count > 0)
+            {
+                //                DataRowView row = (DataRowView)lvUsers.SelectedItems[0];
+                ListViewItem itemx = (ListViewItem)lvUsers.SelectedItems[0];
+
+                NpgsqlCommand cmd = new NpgsqlCommand();
+                conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;Database=test;User Id=postgres;" + "Password=admin;");
+                conn.Open();
+                cmd.Connection = conn;
+                //                int selectedId;
+                //ListViewItem selectedItem = (ListViewItem)sender;
+                //selectedId = (selectedItem.SubItems[0]);
+
+
+                cmd.CommandText = "delete from simple_table where id=" + lvUsers.Items[0];
+
+                cmd.ExecuteNonQuery();
+                BindGrid();
+                MessageBox.Show("Employee Deleted Successfully...");
+            }
+            else
+            {
+                MessageBox.Show("Please Select Any Employee From List...");
+            }
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lvUsers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
+    
 }
